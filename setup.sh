c775cbfef9f093132c738893b2fb77dd0d1ed89a
LordZeuss <87180353+LordZeuss@users.noreply.github.com>
2023-03-22 23:01:23 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -604,9 +604,9 @@ echo " "
 echo " "
 echo -e "\e[1;36mWould you like to configure and hash the admin password automatically or configure it manually yourself? (a|auto | m|manually)\e[0m"
 
-read -n1 yesorno
+read -n1 configure
 
-if [ "$yesorno" = a ]; then
+if [ "$configure" = a ]; then
   echo " "
   read -s -p $'\e[1;36mEnter the password for the admin user\e[0m: ' adminpass
   echo " "
@@ -632,7 +632,7 @@ if [ "$yesorno" = a ]; then
   sed -i "s/HASHPASS/$secret/" /home/$USER/auto-authelia/authelia/config/users_database.yml
   echo " "
   echo -e "\e[1;32mPassword Updated.\e[0m"
-elif [ "$yesorno" = m ]; then
+elif [ "$configure" = m ]; then
   echo " "
   echo "users:
   $user: #username for user 1. change to whatever you'd like
